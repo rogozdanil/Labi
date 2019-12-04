cbf22a0555a82e19c510b5b2017df5cbf6e8b4a4
danilka <danilka@783B>
2019-12-04 16:26:13 +0300

    Add all Labaaaaaaaaaaaa

---
+++ Labaa6_p1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaa6_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
