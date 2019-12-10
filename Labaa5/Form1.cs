d351c032c09bc1669581a0e74d3b81ca8ca54112
danilka <danilka@783B>
2019-12-10 15:49:53 +0300

    Добавил библиотеку в 5 лабу

--- Labaa5/Form1.cs
+++ Labaa5/Form1.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using Labaa5_library;
 
 namespace Labaa5
 {
@@ -19,49 +15,6 @@ namespace Labaa5
             InitializeComponent();
         }
         List<string> list = new List<string>();
-        public static int Distance(string str1Par, string str2Par)
-        {
-            if ((str1Par == null) || (str2Par == null)) return -1;
-            int str1Len = str1Par.Length;
-            int str2Len = str2Par.Length;
-
-            if ((str1Len == 0) && (str2Len == 0)) return 0;
-            if (str1Len == 0) return str2Len;
-            if (str2Len == 0) return str1Len;
-
-            string str1 = str1Par.ToUpper();
-            string str2 = str2Par.ToUpper();
-            int[,] matrix = new int[str1Len + 1, str2Len + 1];
-
-            //Инициализация нулевой строки и нулевого столбца матрицы
-            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
-            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
-        
-            for (int i = 1; i <= str1Len; i++)
-            {
-                for (int j = 1; j <= str2Len; j++)
-                {
-                    //Эквивалентность символов
-                    int symbEqual = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
-                    //Возможные операции
-                    int ins = matrix[i, j - 1] + 1;
-                    int del = matrix[i - 1, j] + 1;
-                    int subst = matrix[i - 1, j - 1] + symbEqual;
-
-                    //Элемент матрицы вычисляется как минимальный из трех случаев
-                    matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
-
-                    //Дополнение Дамерау
-                    if ((i > 1) && (j > 1) && (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
-                        (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
-                    {
-                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + symbEqual);
-                    }
-                }
-            }
-            //Нижний правый элемент матрицы
-            return matrix[str1Len, str2Len];
-        }
         private void reading_button_Click(object sender, EventArgs e)
         {
             OpenFileDialog fd = new OpenFileDialog();
@@ -108,7 +61,8 @@ namespace Labaa5
 
                 foreach (string str in list)
                 {
-                    if (Distance(wordUpper, str.ToUpper()) <= int.Parse(text_dist.Text)) tempList.Add(str);
+                    if (Manager.Distance(wordUpper, str.ToUpper()) <= int.Parse(text_dist.Text)) 
+                        tempList.Add(str);
                 }
 
                 timer.Stop();
---
+++ Labaa5_library/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Labaa5_library
+{
+    public static class Manager
+    {
+        public static int Distance(string str1Par, string str2Par)
+        {
+            if ((str1Par == null) || (str2Par == null)) return -1;
+            int str1Len = str1Par.Length;
+            int str2Len = str2Par.Length;
+
+            if ((str1Len == 0) && (str2Len == 0)) return 0;
+            if (str1Len == 0) return str2Len;
+            if (str2Len == 0) return str1Len;
+
+            string str1 = str1Par.ToUpper();
+            string str2 = str2Par.ToUpper();
+            int[,] matrix = new int[str1Len + 1, str2Len + 1];
+
+            //Инициализация нулевой строки и нулевого столбца матрицы
+            for (int i = 0; i <= str1Len; i++) matrix[i, 0] = i;
+            for (int j = 0; j <= str2Len; j++) matrix[0, j] = j;
+
+            for (int i = 1; i <= str1Len; i++)
+            {
+                for (int j = 1; j <= str2Len; j++)
+                {
+                    //Эквивалентность символов
+                    int symbEqual = ((str1.Substring(i - 1, 1) == str2.Substring(j - 1, 1)) ? 0 : 1);
+                    //Возможные операции
+                    int ins = matrix[i, j - 1] + 1;
+                    int del = matrix[i - 1, j] + 1;
+                    int subst = matrix[i - 1, j - 1] + symbEqual;
+
+                    //Элемент матрицы вычисляется как минимальный из трех случаев
+                    matrix[i, j] = Math.Min(Math.Min(ins, del), subst);
+
+                    //Дополнение Дамерау
+                    if ((i > 1) && (j > 1) && (str1.Substring(i - 1, 1) == str2.Substring(j - 2, 1)) &&
+                        (str1.Substring(i - 2, 1) == str2.Substring(j - 1, 1)))
+                    {
+                        matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + symbEqual);
+                    }
+                }
+            }
+            //Нижний правый элемент матрицы
+            return matrix[str1Len, str2Len];
+        }
+    }
+}
